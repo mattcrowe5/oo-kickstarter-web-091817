dd964e32065bccbb44e21e79531e1945d2dbe514
mattcrowe5 <crowe.matthew5@gmail.com>
2017-09-21 16:24:12 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_reader :title
+  attr_accessor :backers
+
+  def initialize(title)
+    @title = title
+    @backers =[]
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self)
+  end
+
+end
